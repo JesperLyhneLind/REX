02088454b73ac083607b71d192ebcad15a6e9480
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-09-04 15:37:34 +0200

    Create 1m.py

---
+++ REX/1m.py
@@ -0,0 +1,21 @@
+# Simpelt eksempel fra absalon
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 64
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+# Wait a bit while robot moves forward
+sleep(2.5)
+
+# send a stop command
+print(arlo.stop())
