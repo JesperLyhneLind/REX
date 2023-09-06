59ea308fa9069840bc60b2c7bc3b5fb5f997a26f
Jesper <JesperLyhneLind@github.com>
2023-09-06 14:24:28 +0200

    ok square

--- REX/square.py
+++ REX/square.py
@@ -11,13 +11,13 @@ print("Running ...")
 
 # send a go_diff command to drive forward
 
-for i in range(4):
+for i in range(12):
     
-    leftSpeed = 64
-    rightSpeed = 64.5
     #1m forward
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-    sleep(2.55)
+    print(arlo.go_diff(33, 33, 1, 1))
+    sleep(0.25)
+    print(arlo.go_diff(64, 64.3, 1, 1))
+    sleep(0.95)
     print(arlo.stop())
     sleep(0.041)
 
@@ -26,33 +26,9 @@ for i in range(4):
 
     # rotate 90 degrees
     print(arlo.go_diff(leftSpeed, rightSpeed, 0,1))
-    sleep(1.4)# Wait a bit while robot moves backwards
+    sleep(0.45)# Wait a bit while robot moves backwards
     print(arlo.stop())
-    sleep(2.141)
-
-
-
-
-# request to read Front sonar ping sensor
-print("Front sensor = ", arlo.read_front_ping_sensor())
-sleep(0.041)
-
-
-# request to read Back sonar ping sensor
-print("Back sensor = ", arlo.read_back_ping_sensor())
-sleep(0.041)
-
-# request to read Right sonar ping sensor
-print("Right sensor = ", arlo.read_right_ping_sensor())
-sleep(0.041)
-
-# request to read Left sonar ping sensor
-print("Left sensor = ", arlo.read_left_ping_sensor())
-sleep(0.041)
-
-
-
-
+    sleep(0.041)
 
 
 
