559b25a1faf34ffd5a8dd13739d7d7743369f965
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-09-11 15:22:13 +0200

    s

---
+++ REX/nyfil.py
@@ -0,0 +1,7 @@
+print("Front sensor = ", arlo.read_front_ping_sensor())
+# request to read Back sonar ping sensor
+print("Back sensor = ", arlo.read_back_ping_sensor())
+# request to read Right sonar ping sensor
+print("Right sensor = ", arlo.read_right_ping_sensor())
+# request to read Left sonar ping sensor
+print("Left sensor = ", arlo.read_left_ping_sensor())
