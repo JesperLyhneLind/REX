443167768c2f013ec9e6c649bc568ca1fc4bb1e7
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-09-11 15:39:36 +0200

    s

--- REX/nyfil.py
+++ REX/nyfil.py
@@ -1,14 +1,37 @@
 import time
 from time import sleep
 import robot
-# Create a robot object and initialize
+
 arlo = robot.Robot()
+
 sleep(1)
-# request to read Front sonar ping sensor
-print("Front sensor = ", arlo.read_front_ping_sensor())
-# request to read Back sonar ping sensor
-print("Back sensor = ", arlo.read_back_ping_sensor())
-# request to read Right sonar ping sensor
-print("Right sensor = ", arlo.read_right_ping_sensor())
-# request to read Left sonar ping sensor
-print("Left sensor = ", arlo.read_left_ping_sensor())
+Front_sensor = arlo.read_front_ping_sensor()
+Right_sensor = arlo.read_right_ping_sensor()
+Left_sensor = arlo.read_left_ping_sensor()
+
+def check():
+    while(True):
+        Front_sensor = arlo.read_front_ping_sensor()
+        Right_sensor = arlo.read_right_ping_sensor()
+        Left_sensor = arlo.read_left_ping_sensor()
+        
+        if Left_sensor < 150 or Right_sensor < 150 or Front_sensor < 150:
+            print(arlo.stop())
+            sleep(0.45)
+
+
+def drive(): 
+    arlo.go_diff(40, 40, 1, 1)
+    check()
+    if Left_sensor < Right_sensor:
+        print(arlo.go_diff(41, 41, 0, 1))
+        sleep(0.45)# Wait a bit while robot moves backwards
+        print(arlo.stop())
+        sleep(0.041)
+    else:
+        print(arlo.go_diff(41, 41, 1, 0))
+        sleep(0.45)# Wait a bit while robot moves backwards
+        print(arlo.stop())
+        sleep(0.041)
+    
+        
