b3a3e442d0647d0caa28e7f13106bc1704ae437e
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-09-11 15:22:46 +0200

    Update nyfil.py

--- REX/nyfil.py
+++ REX/nyfil.py
@@ -1,3 +1,6 @@
+arlo = robot.Robot()
+sleep(1)
+# request to read Front sonar ping sensor
 print("Front sensor = ", arlo.read_front_ping_sensor())
 # request to read Back sonar ping sensor
 print("Back sensor = ", arlo.read_back_ping_sensor())
