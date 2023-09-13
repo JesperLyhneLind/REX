8baf905f18db9e711d8b1e2e09b7c397acad2606
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-09-13 14:26:50 +0200

    sonarcalibration

--- REX/sonarcalibration.py
+++ REX/sonarcalibration.py
@@ -9,6 +9,8 @@ class Direction(Enum):
     Left = 1
     Right = 2
 
-# Checks if there's any object in the path of the robot.  
-Front_sensor = arlo.read_front_ping_sensor()
-print("Front: " + str(Front_sensor))
+# Checks if there's any object in the path of the robot. 
+for i in range(7): 
+    Front_sensor = arlo.read_front_ping_sensor()
+    print("Front: " + str(Front_sensor))
+    sleep(2)
