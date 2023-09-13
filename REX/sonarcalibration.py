e5ab9d0a8c32d40ef7b86ddfdf2fe6b5da996f96
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-09-13 13:10:25 +0200

    Create sonarcalibration.py

---
+++ REX/sonarcalibration.py
@@ -0,0 +1,14 @@
+import time
+from time import sleep
+import robot
+from enum import Enum
+
+arlo = robot.Robot()
+
+class Direction(Enum):
+    Left = 1
+    Right = 2
+
+# Checks if there's any object in the path of the robot.  
+Front_sensor = arlo.read_front_ping_sensor()
+print("Front: " + str(Front_sensor))
