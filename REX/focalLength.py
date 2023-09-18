3e836ea7ae19e535cddd6062e08aa0b6109d3df4
cfp185 <cfp185@alumni.ku.dk>
2023-09-18 15:58:16 +0200

    test

--- REX/focalLength.py
+++ REX/focalLength.py
@@ -51,8 +51,8 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     cv2.imshow(WIN_RF, frameReference)
     if cv2.waitKey(4) == 32: #takes picture when pressing space
         print("pressed space")
-        path = '/Pictures'
-        filename = 'Pictures/' + str(cnt) + '.jpg'
+        path = '../REX/REX/pictures'
+        filename = 'pictures/' + str(cnt) + '.jpg'
         cv2.imwrite(filename, frameReference)
         cnt += 20
 
