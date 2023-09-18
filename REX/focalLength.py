631c83541e9c31fa8783202560ace8652e0d2009
cfp185 <cfp185@alumni.ku.dk>
2023-09-18 15:18:48 +0200

    focalLength.py

--- REX/focalLength.py
+++ REX/focalLength.py
@@ -1,8 +1,4 @@
-# This script shows how to open a camera in OpenCV and grab frames and show these.
-# Kim S. Pedersen, 2022
-
 import cv2 # Import the OpenCV library
-
 
 def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
     """Utility function for setting parameters for the gstreamer camera pipeline"""
@@ -19,44 +15,29 @@ def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
         )
     )
 
-
 print("OpenCV version = " + cv2.__version__)
 
 # Open a camera device for capturing
 cam = cv2.VideoCapture(gstreamer_pipeline(), apiPreference=cv2.CAP_GSTREAMER)
-
 
 if not cam.isOpened(): # Error
     print("Could not open camera")
     exit(-1)
 
 # Open a window
-WIN_RF = "Example 1"
+WIN_RF = "Focal Length"
 cv2.namedWindow(WIN_RF)
 cv2.moveWindow(WIN_RF, 100, 100)
 
-
-#while cv2.waitKey(4) == -1: # Wait for a key pressed event
-#    retval, frameReference = cam.read() # Read frame
-#    
-#    if not retval: # Error
-#        print(" < < <  Game over!  > > > ")
-#        exit(-1)
+while cv2.waitKey(4) == -1: # Wait for a key pressed event
+    retval, frameReference = cam.read() # Read frame
+    
+    if not retval: # Error
+        print(" < < <  Game over!  > > > ")
+        exit(-1)
     
     # Show frames
-#    cv2.imshow(WIN_RF, frameReference)
+    cv2.imshow(WIN_RF, frameReference)
 
-cnt = 40
-while cv2.waitKey(4) == -1: # Wait for a key pressed event
-    image = cam.capture_array("main")
-    # Show frames
-    cv2.imshow(WIN_RF, image)
-    if cv2.waitKey(4) == 32: #takes picture when pressing space
-        for i in range(5):
-            print("pressed space")
-            path = '/Pictures'
-            filename = 'Pictures/' + str(cnt) + str(i) + '.jpg'
-            cv2.imwrite(filename, image)
-        cnt += 20    
 
 # Finished successfully
