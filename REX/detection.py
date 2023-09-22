4145c05659c881f83c8f79ef1bfa5099419f9bdf
cfp185 <cfp185@alumni.ku.dk>
2023-09-22 13:44:54 +0200

    det

--- REX/detection.py
+++ REX/detection.py
@@ -5,7 +5,6 @@ from cv2 import aruco
 #import matplotlib as mpl
 #import pandas as pd
 import time
-import imutils
 
 
 try:
@@ -38,7 +37,6 @@ cv2.moveWindow(WIN_RF, 100, 100)
 # while cv2.waitKey(4) == -1: # Wait for a key pressed event
 #image = cam.capture_array("main") # Read frame
 image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
 
     # if not retval: # Error
     #     print("):< ):< ):< Error >:( >:( >:(")
