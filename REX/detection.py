588b9129ebb478ae133952d620f4ec39a25be350
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-09-25 15:34:07 +0200

    Update detection.py

--- REX/detection.py
+++ REX/detection.py
@@ -1,9 +1,6 @@
 import numpy as np
 import cv2
 from cv2 import aruco
-#import matplotlib.pyplot as plt
-#import matplotlib as mpl
-#import pandas as pd
 import time
 
 
@@ -49,13 +46,6 @@ aruco_type = aruco.DICT_6X6_250
 #id = 2
 aruco_dict = aruco.Dictionary_get(aruco_type)
 
-#print("ArUCo type '{}' with ID '{}".format(aruco_type, id))
-#tag_size = 600
-#tag = np.zeros((tag_size, tag_size, 1), dtype="uint8")
-#aruco.drawMarker(aruco_dict, id, tag_size, tag, 1)
-
-#tag_name = "arucoMarkers/" + str(aruco_type) + "_" + str(id) + ".png"
-#cv2.imwrite(tag_name, tag)
 
 while cv2.waitKey(4) == -1: # Wait for a key pressed event
     retval, frameReference = cam.read() # Read frame
@@ -67,7 +57,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     # Show frames
     cv2.imshow(WIN_RF, frameReference)
 
-    params = cv2.aruco.DetectorParameters_create()
+    params = aruco.DetectorParameters_create()
     corners, ids, rejected_corners = aruco.detectMarkers(frameReference, aruco_dict, parameters=params)
     #print(aruco.detectMarkers(frameReference, aruco_dict, params))
     #print("ids: ", ids)
@@ -81,27 +71,19 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     rvecs, tvecs, objPoints = aruco.estimatePoseSingleMarkers(corners, 145, camMatrix, None, None)
     print("tvecs: ", tvecs)
 
+    angle = np.arccos(np.dot((tvecs/abs(tvecs)), [0, 0, 1]))
 
-#markerlength = 14,5 cm
-#distcoeff = none
-  #  text = input('')    
- #   if text == 'q':
-#        break
 
-#cv2.imshow("ArUCo Tag", tag)
 
-#cv2.waitKey(0)
 
-#cv2.destroyAllWindows()
 
-#fig = plt.figure()
-#nx = 4
-#ny = 3
-#for i in range(1, nx*ny+1):
-#    ax = fig.add_subplot(ny,nx, i)
-#    img = aruco.drawMarker(aruco_dict,i, 700)
-#    plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
-#    ax.axis("off")
 
-#plt.savefig("_data/markers.pdf")
-#plt.show()
+
+
+#print("ArUCo type '{}' with ID '{}".format(aruco_type, id))
+#tag_size = 600
+#tag = np.zeros((tag_size, tag_size, 1), dtype="uint8")
+#aruco.drawMarker(aruco_dict, id, tag_size, tag, 1)
+
+#tag_name = "arucoMarkers/" + str(aruco_type) + "_" + str(id) + ".png"
+#cv2.imwrite(tag_name, tag)
