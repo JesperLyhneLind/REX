f9231dd4dd2d02fcd2b7f8b187796edc5f24ca18
JesperLyhneLind <jesperlyhnelind@icloud.com>
2023-09-25 16:25:37 +0200

    Update detection.py

--- REX/detection.py
+++ REX/detection.py
@@ -2,7 +2,11 @@ import numpy as np
 import cv2
 from cv2 import aruco
 import time
+from time import sleep
+import robot
+from enum import Enum
 
+arlo = robot.Robot()
 
 try:
     import picamera2
@@ -46,6 +50,33 @@ aruco_type = aruco.DICT_6X6_250
 #id = 2
 aruco_dict = aruco.Dictionary_get(aruco_type)
 
+class Direction(Enum):
+    Left = 1
+    Right = 2
+
+# Turns the robot angle degrees.
+def turn(dir: Direction, angle: int):
+    if dir == Direction.Left:
+        print(arlo.go_diff(49, 49, 0, 1))
+        sleep(angle/90)
+        print(arlo.stop())
+        sleep(0.041)
+    else:
+        print(arlo.go_diff(49, 49, 1, 0))
+        sleep(angle/90)
+        print(arlo.stop())
+        sleep(0.041)
+
+# Drives one meter.
+def driveM(meters):
+    leftSpeed = 70
+    rightSpeed = 70
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    # Wait a bit while robot moves forward
+    sleep(1.7*meters)
+    # send a stop command
+    print(arlo.stop())   
+    sleep(0.041)  
 
 while cv2.waitKey(4) == -1: # Wait for a key pressed event
     retval, frameReference = cam.read() # Read frame
@@ -75,12 +106,24 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     z_vector = np.array([0, 0, 1])
 
     if tvecs is not None:
-        norm_tvecs = np.linalg.norm(tvecs)
-        dot = np.dot((tvecs / norm_tvecs), z_vector)
-        angle = np.arccos(dot)
+        dist = np.linalg.norm(tvecs) #distance to the box
+        dot = np.dot((tvecs / dist), z_vector)
+        angle = np.degrees(np.arccos(dot))
         print("angle: ", angle)
         print("tvec norm", np.linalg.norm(tvecs))
-        angle_sign = np.sign(tvecs[0])
+        angle_sign = np.sign(tvecs[0]) # 1 is right, -1 is left
+    
+        if angle_sign == -1:
+            turn(Direction.Left, angle)
+            driveM((dist - 500) / 100)
+        elif angle_sign == 1:
+            turn(Direction.Right, angle)
+            driveM((dist - 500) / 100)
+        else:
+            driveM((dist - 500) / 100)
+
+
+
 
     
 
