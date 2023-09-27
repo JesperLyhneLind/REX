cc1ed64ad76dc23a963e9b4514dda54bbe9fea66
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-09-27 10:57:13 +0200

    s

--- REX/detection.py
+++ REX/detection.py
@@ -119,17 +119,12 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
         dist = np.linalg.norm(tvecs) #distance to the box
         dot = np.dot((tvecs / dist), z_vector)
         angle = np.degrees(np.arccos(dot))
-        # print("angle: ", angle)
-        # print("tvec norm", np.linalg.norm(tvecs))
-        angle_sign = np.sign(int(tvecs[0])) # 1 is right, -1 is left
-        
-        # print("angle sign: ", angle_sign)
-        # print("angle sign[0]: ", angle_sign[0])
-        print("angle sign[0][0]: ", int(angle_sign[0][0]))
+        angle_sign = np.sign(tvecs[0]) # 1 is right, -1 is left
+        print("angle sign[0][0]: ", int((angle_sign[0])[0]))
         # print("t_vecs: ", tvecs)
         # print("t_vecs[0]: ", tvecs[0])
 
-        go_to_box(int(angle_sign[0][0]), angle, dist, ids)
+        go_to_box(int((angle_sign[0])[0]), angle, dist, ids)
 
     else:
         turn(Direction.Right, 45)
