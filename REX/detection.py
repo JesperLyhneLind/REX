e7fc761ba28ba6213e3c9a29f0d2f3c756db2c49
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-09-27 12:57:52 +0200

    asd

--- REX/detection.py
+++ REX/detection.py
@@ -112,11 +112,12 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     z_vector = np.array([0, 0, 1])
 
     if tvecs is not None:
+        print(arlo.stop()) 
         norms = []
         for i in range(len(tvecs)):
             norms.append(np.linalg.norm(tvecs[i]))
         maxvecidx = int(norms.index(min(norms)))
-        vec = (tvecs[norms.index(min(norms))])[0] #choose the closest vector
+        vec = tvecs[norms.index(min(norms))][0] #choose the closest vector
         dist = np.linalg.norm(vec) #distance to the box
         dot = np.dot((vec / dist), z_vector)
         angle = np.degrees(np.arccos(dot))
@@ -127,7 +128,7 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
         print("vec:",vec)
         print("angle:", angle)
         print("ids", ids)
-        go_to_box(angle_sign[0], angle, dist, id[maxvecidx])
+        go_to_box(angle_sign[0], angle, dist, ids[maxvecidx])
 
     else:
         turn(Direction.Right, 45)
